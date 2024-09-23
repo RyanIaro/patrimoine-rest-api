ae5f134a6829ab87075c1922db9574a74a2f27e1
RyanIaro <hei.ryan.3@gmail.com>
2024-09-23 15:23:19 +0300

    feat: add patrimoine model

---
+++ src/main/java/com/rattrapage/patrimoine/model/Patrimoine.java
@@ -0,0 +1,16 @@
+package com.rattrapage.patrimoine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Patrimoine {
+    private String id;
+    private String possesseur;
+    private LocalDateTime derniereModification;
+}
